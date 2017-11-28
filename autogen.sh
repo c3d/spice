2902de95e364c22d5374f62ca11fcc1a6c087af0
Christophe de Dinechin <dinechin@redhat.com>
2017-11-28 17:10:30 +0100

    top-level-spice: Build all of SPICE from top-level
    This is a new repository that makes it easier to build all
    of SPICE with a single 'make' command. It also optimizes
    the build of 'common' and 'protocol', which are built only once
    and then used by both spice-gtk and spice-server. This makes
    it easier and safer to develop code that impacts these shared
    components.
    
    This build system was also designed to be compatible with the
    standard build system. You can run ./autogen.sh, and it will
    switch to auto-tools. I have not tested that feature in a while.
    
    I have not yet attempted to do anything similar with Meson/Ninja.

---
+++ autogen.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+git submodule update --init
+
+echo RECURSE=all                                        >  autogen.mk
+echo SUBDIRS=                                           >> autogen.mk
+
+export PKG_CONFIG_PATH=/usr/local/share/pkgconfig:$PKG_CONFIG_PATH
+
+BUILD_STEP=
+if [ "x$1" != "x" ]; then
+    BUILD_STEP="$1"
+    shift
+fi
+
+for DIR in protocol common clients/gtk server streaming-agent
+do
+    echo Entering $DIR
+    (cd $DIR && ./autogen.sh ${1+"$@"} && ([ -z "$BUILD_STEP" ] || $BUILD_STEP))
+    echo "AUTOGEN+=$DIR"                            >> autogen.mk
+done
